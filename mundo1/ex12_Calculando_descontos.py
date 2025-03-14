0e5ac548dd9b70e10d723cabb7a7ffe5c7274298
tiago coser <cosertiago@gmail.com>
2025-03-14 17:28:15 -0300

    conti...

--- mundo1/ex12_Calculando_descontos.py
+++ mundo1/ex12_Calculando_descontos.py
@@ -1,1 +1,4 @@
 #Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
+n = float(input('Digite o preço R$ '))
+np = n - (n * 5 / 100)
+print(f'o valor com 5% de desconto é R$ {np:.2f}')
