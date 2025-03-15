1b208e2cba7dee16e3edbed8f3d43a91a94727cd
tiago coser <cosertiago@gmail.com>
2025-03-15 19:30:32 -0300

    fim do mundo 1

--- mundo1/ex34_Aumento_múltiplos.py
+++ mundo1/ex34_Aumento_múltiplos.py
@@ -3,6 +3,9 @@ Escreva um programa que pergunte o salário de um funcionário e calcule o valor
 Para salários superiores a R$1250,00, calcule um aumento de 10%. Para os inferiores ou iguais, o aumento é de 15%.
 
 """
-s = float(input('Digite seu Sálario '))
+s = float(input('Digite seu Sálario R$ '))
 if s > 1.250:
-    aumento = s 
+    ns = s + (s * 10 / 100)
+if s <= 1.250:
+    ns = s + (s * 15 / 100)
+print(f'Seu novo salário é R$ {ns:.3f} reais')
