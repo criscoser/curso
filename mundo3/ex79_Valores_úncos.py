548886ad4146bb36eef4072ea52c3cebf19efb3d
tiago coser <cosertiago@gmail.com>
2025-04-01 10:10:40 -0300

    mundo3....

--- mundo3/ex79_Valores_úncos.py
+++ mundo3/ex79_Valores_úncos.py
@@ -1,2 +1,16 @@
 '''Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma lista. 
 Caso o número já exista lá dentro, ele não será adicionado. No final, serão exibidos todos os valores únicos digitados, em ordem crescente.'''
+numeros = list()
+while True:
+    n = int(input('digite um valor: '))
+    if n not in numeros: 
+        numeros.append(n)
+        print('Valor Adicionado com sucesso...')
+    else:
+        print('valor duplicado! Não vou adicionar...')
+    r = str(input('Quer continuar? [S/N]'))
+    if r in 'Nn':
+        break
+print('=' * 30)
+numeros.sort()
+print(f'você adicionou ps valores {numeros}')
